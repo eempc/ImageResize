14ca6b70b0acda15923edf1e55047203e6eec086
eempc <evc484@gmail.com>
2019-09-16 18:37:38 +0100

    first

---
+++ ImageResize/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace ImageResize {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
